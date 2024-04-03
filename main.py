9c6e363d0c9e17036c0ee2c2e64c956474a7058c
anurshai <anurshai@sfu.ca>
2024-04-02 18:58:38 -0700

    More presized contextual information

--- main.py
+++ main.py
@@ -2,21 +2,25 @@ from dis import Instruction
 import os
 from apikey import apikey
 from cohere import ChatMessage
+from openai import chat
 from sklearn.utils import resample
 import streamlit as st
 from streamlit_extras.grid import grid
 from streamlit_extras.colored_header import colored_header 
 from streamlit_extras.row import row
 from streamlit_extras.dataframe_explorer import dataframe_explorer
+from streamlit_extras.stylable_container import stylable_container
 import pandas as pd
 import re
 import altair as alt
 from pathlib import Path
 from PIL import Image
+from sympy import use
 import helper 
 import logging
 import json
 import time
+import numpy as np
 
 from langchain import hub
 from langchain_openai import ChatOpenAI
@@ -44,12 +48,12 @@ st.title('Dataset Exploration')
 def create_agent_safely(df):
     try:
         if df is not None:
-            llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo")
+            llm = ChatOpenAI(temperature=0, model="gpt-4")
             agent = create_pandas_dataframe_agent(llm,
                                                 df,
                                                 verbose=True,
                                                 agent_type=AgentType.OPENAI_FUNCTIONS,
-                                                handle_parsing_errors=True,)
+                                                handle_parsing_errors=True)
             return agent
         else:
             raise ValueError("DataFrame is not defined.")
@@ -62,7 +66,7 @@ def create_agent_safely(df):
 
 class AgentResponseSchema(BaseModel):
     answer: str = Field(description="The direct answer derived from your the DataFrame analysis. Must be in markdown format")
-    chartPrompt: str = Field(description="If asked to create chart or visualize data, this field provides details on what the chart should represent in detail, including the best possible title. If no chart is necessary, leave this field empty.")
+    chartPrompt: str = Field(description="If asked to create chart or visualize data, this field provides a prompt for another agent to create what chart it should create in detail. This prompt must include the title and type of chart. If no chart is necessary, leave this field empty.")
 
 parser = PydanticOutputParser(pydantic_object=AgentResponseSchema)
 
@@ -70,16 +74,23 @@ def create_chat_prompt(chat_history, user_question):
     chat_messages: List[ChatMessage] = [ChatMessage(role=chat_item["role"], content=chat_item["content"]) for chat_item in chat_history]
 
     prompt = ChatPromptTemplate(
-        messages= chat_messages + [
+        messages= [
             HumanMessagePromptTemplate.from_template(
-                "answer this user question as best as possible. You must return answer using these format instructions\n{format_instructions}\n{question}"
+                """answer this user question as best as possible.
+                Always answer based on the full dataframe, not only head rows
+                You must return answer using these format instructions\n{format_instructions} Question:\n{question}"""
             )
         ],
         input_variables=["question"],
         partial_variables={
-            "format_instructions": parser.get_format_instructions(),
+            "format_instructions": """The output must be formatted as a JSON
+                                        instance that conforms to the JSON schema below.
+                                        As an example, the schema {"answer": "The dataset has x number of rows",
+                                        "chartPrompt": "Descriptive chart creation prompt for another agent"} \nthe json 
+                                        is a well-formatted instance of the schema. The object {"answer": {"foo": ["bar", "baz"]}} is not well-formatted.""",
         },
     )
+    print(prompt.format_prompt(question=user_question).to_messages())
 
     input_messages = prompt.format_prompt(question=user_question).to_messages()
 
@@ -105,7 +116,7 @@ def parse_response(response_text) -> AgentResponseSchema:
         return AgentResponseSchema(answer=response_text, isChart=False, chartPrompt="")
     
 
-def generate_chart(prompt) -> Image:
+def generate_chart(prompt, df) -> Image:
     lida = Manager(text_gen = lidallm("openai"))
     textgen_config = TextGenerationConfig(n=1, temperature=0.2, use_cache=True)
     summary = lida.summarize(df, summary_method="default", textgen_config=textgen_config)
@@ -125,25 +136,24 @@ with col1:
     # Load data
     uploaded_file = st.file_uploader("Upload your dataset", type=['csv'])
 
-    df = None
+    df = None  
     agent = None
     if uploaded_file is not None:
-        # Read the uploaded CSV file with Pandas
         df = pd.read_csv(uploaded_file)
         
         # Display the DataFrame
         st.subheader("Current dataframe:", divider='blue')
-        st.data_editor(df, height=545)  
-        # st.dataframe(dataframe_explorer(df), use_container_width=True)
-        
-        agent = create_agent_safely(df)
+        filtered_df = dataframe_explorer(df, case=False)
+        st.data_editor(filtered_df, height=545)  
+        agent = create_agent_safely(filtered_df)
 
 with col2:
+    suggestions_toggle = st.toggle("Enable suggestions", False)
     # To draw the border line
     container = st.container(border=True)
     with container.container():
 
-        chat_grid = grid(1, 1,  vertical_align="bottom")
+        chat_grid = grid(1, [1, 1], [7, 1],  vertical_align="center")
         with chat_grid.container(height = 700, border=False):
 
             # Messages
@@ -161,8 +171,41 @@ with col2:
                     with st.chat_message(message["role"]):
                         st.markdown(message["content"]) 
 
+            # Suggestions
+            if agent and suggestions_toggle:
+                with chat_grid.container():
+                    with stylable_container(
+                    key="clear_chdaat",
+                    css_styles="""
+                        button {
+                            background-color: black;
+                            color: white;
+                            border-radius: 20px;
+                        }
+                        """,
+                    ):
+                        st.button("Clear chasftlksjhdlkfghsklfjghlksjhdglkjchasfftlksjhdlkfghsklfjghlksjhdglkj", key="clear_chdaat", use_container_width=True)
+                with chat_grid.container():    
+                    with stylable_container(
+                    key="clear_chadt",
+                    css_styles="""
+                        button {
+                            background-color: black;
+                            color: white;
+                            border-radius: 20px;
+                        }
+                        """,
+                    ):
+                        st.button("Clear chatschasdstlksjhdlkfghsklfjghlksjhdglkj", key="clear_chadt", use_container_width=True)
+            else: 
+                chat_grid.empty()
+                chat_grid.empty()
+
+
             # Chat input for new questions 
             prompt = chat_grid.chat_input("Message assistant...")
+            chat    = chat_grid.button("Clear chat", key="clear_chat")
+
 
             # Display welcome message
             if prompt == None:
@@ -201,7 +244,7 @@ with col2:
                     with st.spinner("Creating chart..."):
                         for attempt in range(3):  # Allows up to 3 attempts
                             try:
-                                chart = generate_chart(agentResponse.chartPrompt)
+                                chart = generate_chart(agentResponse.chartPrompt, filtered_df)
                                 break
                             except Exception as e:
                                 logging.error("Chart generation: %s", str(e))
